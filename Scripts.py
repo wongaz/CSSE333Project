d9408214c0329461cdf53e0b8f849b9df47a0996
wongaz <wongaz@rose-hulman.edu>
2017-05-11 16:09:48 -0400

    attempt 1 to run matching

--- Scripts.py
+++ Scripts.py
@@ -13,13 +13,37 @@ app.config['MYSQL_DATABASE_DB'] = 'majormatch'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
+class Node:
+
+    def __init__(self,UserID, Value):
+        self.Profile = UserID
+        self.score = Value
+
 
 @app.route('/')
 def hello():
     print("Connected!")
+    matchBatch()
     return render_template("login.html")
 
 
+def matchBatch():
+    connection = mysql.connect()
+    cursor = connection.cursor()
+    cursor.execute("SELECT email from People")
+    AllEmails= cursor.fetchall()
+    #for k in range(len(AllEmails)):
+    #currentEmail = AllEmails[0]
+    currentEmail = "Ldh@redred.com"
+    cursor.callproc('getDesiredProfile', (str(currentEmail),))
+    currentProfile=cursor.fetchall()
+    cursor.callproc('getOtherProfiles', (str(currentEmail),))
+    otherProfiles = cursor.fetchall()
+    tupleProfileList=matching(currentProfile[0], otherProfiles)
+    for k in range(len(tupleProfileList)):
+        cursor.callproc('addMatch', (tupleProfileList[k].Profile,tupleProfileList[k].Score,currentProfile[0]),)
+
+    connection.commit()
 @app.route('/Authenticate', methods=['POST'])
 def authenticate():
     print('Authentication Starting')
@@ -189,7 +213,6 @@ def postRegister():
     connection.commit()
     return render_template('login.html')
 
-
 @app.route('/Logout',methods = ['POST'])
 def Logout():
     print("Logout")
@@ -250,6 +273,88 @@ def SavePref():
                            week_bed=str(records[0][13]),
                            week_wake=str(records[0][14]))
 
+
+def matching(DesiredAttributes, AllAttributes):
+    List = []
+    DesiredAcademicStatus = DesiredAttributes[1]
+    DesiredGPA  = DesiredAttributes[2]
+    DesiredSH = DesiredAttributes[3]
+    DesiredAU = DesiredAttributes[4]
+    DesiredCU = DesiredAttributes[5]
+    DesiredVU = DesiredAttributes[6]
+    DesiredHC = DesiredAttributes[7]
+    DesiredEth = DesiredAttributes[8]
+    DesiredGender = DesiredAttributes[9]
+    DesiredHE = DesiredAttributes[10]
+    DesiredWeekendSt = DesiredAttributes[11]
+    DesiredWeekendE = DesiredAttributes[12]
+    DesiredWeekSt = DesiredAttributes[13]
+    DesiredWeekE = DesiredAttributes[14]
+    DesiredMajorID = DesiredAttributes[15]
+    DesiredMajorName = DesiredAttributes[16]
+
+
+    for k in range(len(AllAttributes)):
+        Attributes = AllAttributes[k]
+        Score = 0
+        OtherAcademicStatus = Attributes[1]
+        OtherGPA = Attributes[2]
+        OtherSH = Attributes[3]
+        OtherAU = Attributes[4]
+        OtherCU = Attributes[5]
+        OtherVU = Attributes[6]
+        OtherHC = Attributes[7]
+        OtherEth = Attributes[8]
+        OtherGender = Attributes[9]
+        OtherHE = Attributes[10]
+        OtherWeekendSt = Attributes[11]
+        OtherWeekendE = Attributes[12]
+        OtherWeekSt = Attributes[13]
+        OtherWeekE = Attributes[14]
+        OtherMajorID = Attributes[15]
+        OtherMajorName = Attributes[16]
+        #GPA
+        if(OtherGPA >DesiredGPA):
+            Score+=2
+        #StudyHabits
+        if(DesiredSH==OtherSH):
+            Score+=1
+        #Academic
+        if DesiredAcademicStatus == OtherAcademicStatus:
+            Score+=2
+        #Alcohol
+        if(DesiredAU == OtherAU):
+            Score+=1
+        #Cigs
+        if (DesiredCU == OtherCU):
+            Score += 1
+        #Vape
+        if (DesiredVU == OtherVU):
+            Score += 1
+        #Hair Color
+        if (DesiredHC == OtherHC):
+            Score += 1
+        #Ethnicity
+        if (DesiredEth == OtherEth):
+            Score += 1
+        #Height
+        if (DesiredHE < OtherHE):
+            Score += 1
+        #Weekend Intersection
+        if((OtherWeekendSt <= DesiredWeekendSt <= OtherWeekendE ) or (DesiredWeekendSt <= OtherWeekendSt <= DesiredWeekendE)):
+            Score+=4
+        #WeekDay Intersection
+        if ((OtherWeekSt <= DesiredWeekSt <= OtherWeekE) or (DesiredWeekSt <= OtherWeekSt <= DesiredWeekE)):
+            Score+=6
+        if (DesiredGender == OtherGender):
+            Score += 2
+        if (DesiredAcademicStatus == OtherAcademicStatus):
+            Score += 2
+
+        newNode = Node(Attributes[0],Score)
+        List.append(newNode)
+
+    return list
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
